18816954e95d89dc98234f922c1d20aaf1d2957c
Abu Bakar Zahid <43203138+AZChouhadry@users.noreply.github.com>
2020-04-16 13:53:15 +0500

    Difference between WriteLine and Write Functions

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharpbeginner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here I called WriteLine Function");
+            Console.Write("This is write function called");
+            
+
+        }
+    }
+}
