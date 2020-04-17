c2bde931b2392cc7e86312c7d618857aa32f5123
Abu Bakar Zahid <43203138+AZChouhadry@users.noreply.github.com>
2020-04-17 12:14:31 +0500

    I have started Classes in C Sharp

--- Program.cs
+++ Program.cs
@@ -1,28 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Csharpbeginner
 {
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My Name is " + firstName + " " + lastName);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            /* 26: started Section 4 non Premitive type
-             
-             classes 
-             structures
-             enums 
-             arrays
-             strings 
-
-            these are non premitive types in CHash
-             
-             
+            /* 
              
              */
+            
         
 
         }
