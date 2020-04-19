24fca25a120f50110ff582c7ac573454ad013afe
Abu Bakar Zahid <43203138+AZChouhadry@users.noreply.github.com>
2020-04-19 19:07:12 +0500

    Completed Lesson 52 Arrays

--- Program.cs
+++ Program.cs
@@ -8,7 +8,17 @@ namespace Csharpbeginner
     {
         /* section <6> Arrays and Lists*/
 
-        /*Completed Lesson 51 Introduction*/
+        static void Main()
+        {
+            Array nmber = new int[4];
+            Array numbrss = new int[4] { 1, 2, 3, 4 };
+
+            /* section <6> Rectangular Array*/
+            Array rect_Arrey = new int[4, 3];
+
+            /* section <6> Jagged Array*/
+            Array jagged_Arrey = new int[4][];
+        }
 
 
     }
