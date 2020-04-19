7b743ac0f5bdd04f4a0fdf4fd04234778e6a5a3c
Abu Bakar Zahid <43203138+AZChouhadry@users.noreply.github.com>
2020-04-19 15:02:03 +0500

    started  section <6> Arrays and Lists

--- Program.cs
+++ Program.cs
@@ -3,26 +3,9 @@
 
 namespace Csharpbeginner
 {
-    public class Person
-    {
-        public string firstName;
-        public string lastName;
-
-        public void Introduce()
-        {
-            Console.WriteLine("My Name is " + firstName + " " + lastName);
-        }
-    }
+   
     class Program
     {
-        static void Main(string[] args)
-        {
-            /* 
-             
-             */
-            
-        
-
-        }
+      /*  started  section <6> Arrays and Lists*/
     }
 }
