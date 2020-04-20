ad1b1267fb3e9007601b561fa0d455d5753fead2
Abu Bakar Zahid <43203138+AZChouhadry@users.noreply.github.com>
2020-04-20 21:52:49 +0500

    Started Section 7 Working with Dates Completed Lesson 58 Introduction

--- Program.cs
+++ Program.cs
@@ -1,37 +1,24 @@
-using System;
-using System.Collections.Generic;
-namespace CsharpBeginner
-{
+﻿using System;
 
+
+namespace Csharpbeginner
+{
+   
     class Program
     {
+        /* section <7> Working with Dates*/
+
         static void Main(string[] args)
-        {   
-            /*Section 6 Arrays and Lists */
+        {
 
-            var num = new [] {2,3,7,4,5 };
-            var num = new List<int>() { 1,2,3,4};
-            num.Add(1);
-            num.AddRange(new int[4] {1, 5,6,7});
 
-            Console.WriteLine("Length:" + num.Length);
-            var index=Array.IndexOf(num, 9);
-            Console.WriteLine(index);
+            /*Started Lesson 58 Introduction*/
 
-            Array.Clear(num, 0, 2);
-            Console.WriteLine("arr[0]:{0},arr[2]:{1}", num[0], num[1]);
-            int [] num2 = new int [3];
-            Array.Copy(num,num2,3);
-            foreach(var a in  num2)
-            {
-                Console.WriteLine(a);
-            }
-            Array.Sort(num);
-            foreach (var a in num)
-            {
-            foreach(var a in num)
-                Console.WriteLine(a);
-            }
-          }
-          }  
+
+        }
+
+
+    }
+
+
 }
