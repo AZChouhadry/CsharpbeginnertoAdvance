6acbea9c19ad8cf45f5dcb75337b861053fac594
Abu Bakar Zahid <43203138+AZChouhadry@users.noreply.github.com>
2020-04-21 00:23:58 +0500

    Completed Lesson 64 Demo Strings

--- Program.cs
+++ Program.cs
@@ -10,15 +10,43 @@ namespace Csharpbeginner
 
         static void Main(string[] args)
         {
-            string s = "12345";
-            Console.WriteLine("String s = " + s);
-            int a = int.Parse(s);
-            Console.WriteLine("Parsed String s = " + a);
-            int b = Convert.ToInt32(a);
-            Console.WriteLine("Converted String a = " + a);
+            var my_name = " Punjab Polce";
+            Console.WriteLine("Trim: '{0}'", my_name.Trim());
+            Console.WriteLine("Trim: '{0}'", my_name.Trim().ToUpper());
 
-           
-            /*Completed Lesson 63 Strings */
+            var index = my_name.IndexOf(' ');
+            var firstName = my_name.Substring(0, index);
+            var lastName = my_name.Substring(index + 1);
+
+            Console.WriteLine("First Name:{0}, Last Name:{1}", firstName, lastName);
+
+            var names = my_name .Split(' ');
+            Console.WriteLine("First:" + names[0]);
+            Console.WriteLine("Second:" + names[1]);
+
+            Console.WriteLine(my_name.Replace("Punjab", "Ullu"));
+
+
+            if (String.IsNullOrEmpty(" ".Trim()))
+            {
+                Console.WriteLine("Invalid");
+
+            }
+            if (String.IsNullOrWhiteSpace(" "))
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            var str = "25";
+            var age = Convert.ToSByte(str);
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C"));
+
+            Console.ReadLine();
+
+            /*Completed Lesson 64 Strings */
 
 
         }
