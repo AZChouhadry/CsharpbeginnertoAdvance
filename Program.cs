e674f7b17a74ac6d757f59c8da6c6b1ad48ec636
Abu Bakar Zahid <43203138+AZChouhadry@users.noreply.github.com>
2020-04-20 19:30:34 +0500

    Completed Lesson 55 Demo Lists

--- Program.cs
+++ Program.cs
@@ -1,40 +1,35 @@
-﻿using System;
-
-
-namespace Csharpbeginner
+using System;
+using System.Collections.Generic;
+namespace test
 {
-   
+
     class Program
     {
-        /* section <6> Arrays and Lists*/
-
         static void Main(string[] args)
         {
+            var num = new [] {2,3,7,4,5 };
+            var num = new List<int>() { 1,2,3,4};
+            num.Add(1);
+            num.AddRange(new int[4] {1, 5,6,7});
 
+            Console.WriteLine("Length:" + num.Length);
+            var index=Array.IndexOf(num, 9);
+            Console.WriteLine(index);
 
-            /*Lesson 54 Lists*/
+            Array.Clear(num, 0, 2);
+            Console.WriteLine("arr[0]:{0},arr[2]:{1}", num[0], num[1]);
+            int [] num2 = new int [3];
+            Array.Copy(num,num2,3);
+            foreach(var a in  num2)
+            {
+                Console.WriteLine(a);
+            }
+            Array.Sort(num);
+            foreach (var a in num)
+            {
+            foreach(var a in num)
+                Console.WriteLine(a);
+            }
 
-
-
-            /*Functions Used in the Lists
-             Add();
-             AddRange();
-             Remove();
-             RemoveAt();
-             Index();
-             Contains();
-             Count();
-
-             
-             */
-
-
-
-
-        }
-
-
-    }
-
-
-}
+            Console.ReadLine();
+            
