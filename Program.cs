b4cb86e51d96afe40340b1cddadcc0a40e72e652
Abu Bakar Zahid <43203138+AZChouhadry@users.noreply.github.com>
2020-04-20 11:05:48 +0500

    completed Lesson 54 Lists

--- Program.cs
+++ Program.cs
@@ -10,37 +10,26 @@ namespace Csharpbeginner
 
         static void Main(string[] args)
         {
-            
-            int[] numbrss = new int[6] { 5, 8, 1, 4, 3, 1 };
-
-            Console.WriteLine("Length of the array is " + numbrss.Length);
-            foreach (int a in numbrss)
-                Console.WriteLine(a);
-            Console.WriteLine("Sorted arrey is here :");
-            Array.Sort(numbrss);
-            foreach (int a in numbrss)
-                Console.WriteLine(a);
-            int indx = Array.IndexOf(numbrss,3);
-            Console.WriteLine("Length of the array is " + numbrss.Length);
-            Console.WriteLine("Index of 3 is: " + indx);
-
-            Array.Clear(numbrss, 0, 2);
-            Console.WriteLine("Length of the array is " + numbrss.Length);
-            foreach (int a in numbrss)
-                Console.WriteLine(a);
-
-            int[] another_arrey = new int[5];
-            Array.Copy(numbrss, another_arrey, 4);
-            foreach (int a in another_arrey)
-                Console.WriteLine(a);
 
 
-            
-
-            /* */
+            /*Lesson 54 Lists*/
 
 
-            /* section <6> Jagged Array*/
+
+            /*Functions Used in the Lists
+             Add();
+             AddRange();
+             Remove();
+             RemoveAt();
+             Index();
+             Contains();
+             Count();
+
+             
+             */
+
+
+
 
         }
 
