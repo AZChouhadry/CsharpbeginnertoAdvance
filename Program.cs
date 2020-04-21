46fdb2d7cc84564df7a54616e864a5a638f900ed
Abu Bakar Zahid <43203138+AZChouhadry@users.noreply.github.com>
2020-04-21 15:11:00 +0500

    Completed Lesson 66 String Builder

--- Program.cs
+++ Program.cs
@@ -10,30 +10,21 @@ namespace Csharpbeginner
 
         static void Main(string[] args)
         {
-            var my_sentence = " Punjab Polce is not professional to their duty";
-
-            Console.WriteLine(my_sentence);
-
-            var words = my_sentence.Split(' ');
-            foreach(var word in words)
-            {
-                Console.WriteLine(word);
-            }
-
-            const int maxLenght = 10;
-
-            if(my_sentence.Length< maxLenght)
-            { Console.WriteLine(my_sentence); }
-            else
-            {
-                var totalCharacters = 0;
-                var summaryWords = new List<string>();
-
-            }
+            
 
         
-
-            /*Completed Lesson 65 Summarizing Text */
+            /*String Builder
+             Indexof()
+             LastIndexof()
+             Contains()
+             StartsWith()
+             Append()
+             Replace()
+             Remove()
+             Insert()
+             Clear()
+             */
+            /*Completed Lesson 65 String Builder */
 
 
         }
