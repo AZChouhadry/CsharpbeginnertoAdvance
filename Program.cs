48520473864325b16e6a27fda6c542d874575a41
Abu Bakar Zahid <43203138+AZChouhadry@users.noreply.github.com>
2020-04-23 13:15:04 +0500

    Started CSharp Intermediate Course
    Completed Lesson 1 Introduction

--- Program.cs
+++ Program.cs
@@ -10,24 +10,15 @@ namespace Csharpbeginner
 
         static void Main(string[] args)
         {
-            /*Completed CSharp Beginner Course*/
+            /*Started CSharp Intermediate Course*/
 
             /*
-         Section 1 : Introduction
-         Section 2 : Introdution to C# and .NET Framework
-         Section 3: Primitive Types and Expressions
-         Section 4: Non-Primitive types
-         Section 5: Control Flow
-         Section 6: Arrays and Lists
-         Section 7: Working with Dates
-         Section 8: Working with Text
-         Section 9: Working with Files
-         Section 10: Debugging Applications
+
              */
 
 
 
-            /*Completed CSharp Beginner Course */
+            /*Completed Lesson 1 Introduction */
         }
     }
 }
