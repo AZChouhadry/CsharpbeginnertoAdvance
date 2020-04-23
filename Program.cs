768e7eb45717b936770ffe9f0f50843e6af9b2a1
Abu Bakar Zahid <43203138+AZChouhadry@users.noreply.github.com>
2020-04-23 12:08:49 +0500

    Completed Section 9 Working with Files Completed Lesson 77  Summary

--- Program.cs
+++ Program.cs
@@ -13,21 +13,11 @@ namespace Csharpbeginner
 
 
 
-            /*Section 9 Working with Files*/
+            /*Completed Section 9 Working with Files*/
 
-            /*
-          Demo Path
-             */
-            var path = @"e:\AZ\my_files\az.txt";
-            var dotindex = path.IndexOf(".");
-            var extesion = path.Substring(dotindex);
-            Console.WriteLine("Extension: " + path.GetExtension(path));
-            Console.WriteLine("File Name is : " + path.GetFileName(path));
-            Console.WriteLine("File Name is : " + path.GetFileNameWithoutExtension(path));
+            /**/
 
-            
-
-            /*Completed Lesson 75  Demo Path*/
+            /*Completed Lesson 77  Summary*/
 
 
             
