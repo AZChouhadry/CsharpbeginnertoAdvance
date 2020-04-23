63ab9529e5c18408348ebcc3f0b57554ace062fc
Abu Bakar Zahid <43203138+AZChouhadry@users.noreply.github.com>
2020-04-23 13:00:43 +0500

    Completed Section 10 Debugging Applications Completed Lesson 84 Summary

--- Program.cs
+++ Program.cs
@@ -6,11 +6,11 @@ namespace Csharpbeginner
 
     class Program
     {
-        /* section <8> Strings*/
+        /* */
 
         static void Main(string[] args)
         {
-            /*Started Section 10 Debugging Applications*/
+            /*Completed Section 10 Debugging Applications*/
 
             /*
              F10: Step Over
@@ -23,43 +23,9 @@ namespace Csharpbeginner
             Debug>Windows>Locals
              */
 
-            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
-            var smallest = GetSmallests(numbers, 3);
-            foreach (var number in smallest)
-            {
-                Console.WriteLine(number);
-            }
 
+
+            /*Completed Lesson 84 Summary */
         }
-  
-        public static List<int> GetSmallests(List<int> list, int count)
-        {
-            var smallests = new List<int>();
-            Console.WriteLine(" For Defensive Count should be between 1 and numbers");
-            while (smallests.Count < count)
-            {
-
-                var min = GetSmallest(list);
-                smallests.Add(min);
-                list.Remove(min);
-            }
-
-
-            return smallests;
-        }
-        public static int GetSmallest(List<int> list)
-        {
-            var min = list[0];
-            Console.WriteLine(" For Defensive Count should be between 1 and numbers");
-            for (var i = 1; i < list.Count; i++)
-            {
-                if (list[i] > min)
-                    min = list[i];
-            }
-            return min;
-
-        }
-
-        /*Completed Lesson 83 Locals and Autos Window */
     }
 }
